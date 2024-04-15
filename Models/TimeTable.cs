72178dc01944e2d81f4edd0caf36507cb91df25c
GilesP <gilespoliarco@gmail.com>
2024-04-15 08:56:59 -0400

    added all my views

--- Models/TimeTable.cs
+++ Models/TimeTable.cs
@@ -1,13 +1,21 @@
-﻿namespace stonecutter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace stonecutter.Models
 {
     public class TimeTable
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter your name")]
         public string? ClientName { get; set; }
+        [Required(ErrorMessage = "Enter your address")]
         public string? Address { get; set; }
+        [Required(ErrorMessage = "Enter your phone number")]
         public string? PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Enter your email")]
         public string? Email { get; set; }
+        
         public string? Description {  get; set; }
+        [Required(ErrorMessage = "Enter a date and time")]
         public DateTime DateTime { get; set; }
     }
 }
